731076bcc205ee270ff8a7efe5d37a99a14492a1
aayache-brahim <brahimayache123@gmail.com>
2023-10-18 19:34:15 +0100

    finshing delete operation

--- src/Components/DeleteUser.jsx
+++ src/Components/DeleteUser.jsx
@@ -11,20 +11,23 @@ const DeleteUser = () => {
         context.deleteUser({payload:{
           id:parseInt(params.id)
       }})
+      window.history.back()
 
     }
 
     useEffect(()=>{
       console.log(context.users)
-      console.log(context.users[params.id])%
+      console.log(context.users[params.id])
+      console.log(context.users[parseInt(params.id+1)]?.fullName)
     },[])
 
   return (
     <div>
-        <div class="alert alert-danger" role="alert">
-          <h4 class="alert-heading">Do You Want To delete the user {context.users.lenght > 0 && context.users[params.id]?.fullName}</h4>
+        <div className="alert alert-danger" role="alert">
+          <h4 className="alert-heading">Do You Want To delete the user <span className='fw-bold'>{context.users[parseInt(params.id-1)]?.fullName}</span></h4>
           <hr/>
-          <p class="mb-0">Athis action not </p>
+          <p className="mb-0">You can't go back </p>
+          <button className="btn btn-danger my-2 mt-2" onClick={handleSubmit}>DELETE</button>
         </div>
     </div>
   )
